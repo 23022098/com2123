fd7c5842cd78874bd956eac98ed5f3465004677b
23022098 <127095597+23022098@users.noreply.github.com>
2024-03-15 20:35:52 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    double M,S,R,E;
+
+    cout<< "Enter the total cost of the merchandise:"<<endl;
+    cin>>M;
+    cout<< "Enter the salary of the employees:" << endl;
+    cin>>S;
+    cout<< "Enter the yearly rent:"<<endl;
+    cin>>R;
+    cout<< "Enter the estimated electricity cost:"<<endl;
+    cin>>E;
+
+ double T;
+ T=M+S+R+E;
+
+ double N;
+ N=M*0.1;
+
+double NW;
+  NW=N/0.85;
+
+  double U;
+  U=(NW+T)/M;
+
+
+
+    cout<<"The Markup is:"<<U;
+    return 0;
+}
